aabbb2adc491a3bef658445611cf9fc56eab8b47
manukkutty3090 <manukkutty@live.com>
2025-08-04 06:19:00 +0000

    Added cloud Run commands

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Welcome to Python Flask World V2.0'
+    return 'Welcome to Python Flask World V3.0 from GCP ci-cd pipeline'
 
 
 if __name__ == '__main__':
